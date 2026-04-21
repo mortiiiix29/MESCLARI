6cfcea40d1d3c1895f92ef04af0b0998bea0b4cb
mortiiiix29 <152429193+mortiiiix29@users.noreply.github.com>
2026-04-21 15:49:48 +0200

    Reorganitza el menú econòmic ocult i elimina evidencies-projecte

--- script.js
+++ script.js
@@ -7,14 +7,9 @@ const REF_MODE_ON = 'on';
 const defaultPage = 'index.html';
 const reformaPages = new Set([
   'reforma.html',
-  'marc-economic.html',
-  'pressupost-obertura.html',
-  'costos-operatius.html',
-  'ingressos-temporalitat.html',
-  'punt-mort.html',
-  'rendibilitat-oferta.html',
-  'tresoreria-seguretat.html',
-  'conclusio-economica.html',
+  'estructura-economica.html',
+  'viabilitat-economica.html',
+  'rendibilitat-dels-menus.html',
   'reforma-pressupost.html',
   'reforma-escenaris.html',
 ]);
